03ca7d212ce372a2ba6740f27a929673bc2edce0
Sarobidy-23 <hei.sarobidy@gmail.com>
2022-11-02 19:15:22 +0300

    feat: add login page and method to stock bearer token in localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Login from './page/login/Login';
 
 function App() {
-  return <div> Welcome home </div>;
+  return(
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+  )
 }
 
 export default App;
--- src/context/ApiContext.tsx
+++ src/context/ApiContext.tsx
@@ -4,17 +4,25 @@ import { ContextType } from '../type';
 
 const DataContext = createContext<ContextType>({
   client: axios.create(),
+  isAuthentified: false,
+  setIsAuthentified: (status:boolean)=>{}
 });
 
 export const useDataProvider = () => useContext(DataContext);
 
 export const DataProvider = ({ children }: any) => {
+  const [isAuthentified, setIsAuthentified] = useState<boolean>(false)
   const client: AxiosInstance = axios.create({
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("heiAdmin-token")}`
+    },
     baseURL: 'http://localhost:8080',
   });
 
   const contextValue = {
     client,
+    isAuthentified,
+    setIsAuthentified
   };
 
   return <DataContext.Provider value={contextValue}>{children}</DataContext.Provider>;
