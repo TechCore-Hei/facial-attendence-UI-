54408282d7fe3b178bb497dec3a3cfec6c70f944
Sarobidy-23 <hei.sarobidy@gmail.com>
2022-11-03 13:24:46 +0300

    feat: add login with facial recognition endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Facial from './page/facial/Facial';
 import Login from './page/login/Login';
+import WhomiFace from './page/login/WhomiFace';
 
 function App() {
   return(
       <Routes>
         <Route path="/login" element={<Login/>}/>
-        <Route path="/facial" element={<Facial/>}/>
+        <Route path="/whoamiface" element={<WhomiFace/>}/>
       </Routes>
   )
 }
