0619285d696c5f029c549dfe3f5e150586b5ad10
SoaMariaka19 <hei.soa.2@gmail.com>
2022-11-03 19:18:05 +0300

    feat: modify interface for select an event

--- src/components/EventSelect.tsx
+++ src/components/EventSelect.tsx
@@ -1,28 +1,53 @@
-import React from "react";
-import pict from '../img/logo.png' ;
-import './../css/EventSelect.css'
+import React from 'react';
+import pict from '../img/logo.png';
+import '../css/EventSelect.css';
 
-export default function SelectEvent(){
-   const cours = " WEB1";
-   const hours = "02-11-2022/8:00 - 10:00";
-   const group = "H1";
+export default function SelectEvent() {
+  const cours = ' WEB1';
+  const hours = '02-11-2022/8:00 - 10:00';
+  const group = 'H1';
 
-   return(
-<div className="row">
-   <img src={pict} alt="Logo de HEI " className="hei"/>
-   <div id="eventsearch">
-      <input type="search" name="event" id="event" />
-      <i className="fa fa-search"></i>
-   </div>
-   <div id="detail"> 
-      <div className="course-details">
-         <p id="one"><span> Cours: </span>{cours}</p>
-         <p id="hours"><span>Heure de cours: </span>{hours}</p>
-         <p id="group"><span> Groupe: </span>{group}</p>
-         <h2 className="status"> Terminée  </h2> 
+  return (
+      <div className="container-event__list">
+         <div className='navbar'>
+            <img src={pict} alt="Logo de HEI " className="hei-logo" />
+            <div className='search-event'>
+               <input type="search" name="event" id="event"  className='search-event__input'/>
+               <i className="fa fa-search search-event__icon"/>
+            </div>
+            <i className='fa fa-user'/>
+         </div>
+
+         <div className='event__container'>
+            <div className='create-event'>
+                  <label className='create-event__label'>Créer un évènement</label>
+                  <i className='fa fa-plus user-details'/>
+            </div>
+            <div className="event__element">
+               <p id="one">
+                  <span> Cours: </span>
+                  {cours}
+               </p>
+               <p id="hours">
+                  <span>Heure de cours: </span>
+                  {hours}
+               </p>
+               <p id="group">
+                  <span> Groupe: </span>
+                  {group}
+               </p>
+               <span className="status"> Terminée </span>
+               <div className='event-option__container'>
+                  <div className='event__option'>
+                     <i className='fa fa-ellipsis-v'/>
+                  </div>
+                  <div className='event__option__details'>
+                     <span>details</span>
+                     <span>modifier</span>
+                  </div>
+               </div>
+            </div>
+         </div>
       </div>
-   </div>
-</div> 
-   )
-
+  );
 }
