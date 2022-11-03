d3a0597ae0c02e8668b4f876dc0cdb5944a27317
Sarobidy-23 <hei.sarobidy@gmail.com>
2022-11-03 12:08:23 +0300

    chore: adding modal component

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,27 @@
+import React, { FC, useState } from "react";
+import './Modal.css';
+import { modalPropType } from "../../type";
+
+
+const Modal: FC <Partial<modalPropType>>= (props) => {
+    const {children, isActive, handleModal, title} = props;
+   
+    return (
+	<section className={`${isActive ? 'modal__container':'close'}`}>
+        <button className="modal__controller" 
+            onClick={()=>{handleModal && handleModal()}}>
+            <i className="modal__close fa fa-close"/>
+        </button>
+        <div className={`modal__body${isActive && '__active'}`}>
+            <div className={`modal__content${isActive ? '__active' : ''}`}>	
+                <div className="modal__header">
+                    {title}
+                </div>
+                {children}	          		
+            </div>	
+        </div>
+    </section>
+    )
+}
+
+export default Modal;
