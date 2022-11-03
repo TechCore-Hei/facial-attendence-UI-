d0054c250f93f3281f854b53efa9ecd83576deea
Sarobidy-23 <hei.sarobidy@gmail.com>
2022-11-03 12:11:04 +0300

    chore: adding facial attending method

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Facial from './page/facial/Facial';
 import Login from './page/login/Login';
 
 function App() {
   return(
       <Routes>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/facial" element={<Facial/>}/>
       </Routes>
   )
 }
