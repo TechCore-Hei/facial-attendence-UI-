55a199c0ae55d74ab40f0a46d6bc86239919de3e
SoaMariaka19 <hei.soa.2@gmail.com>
2022-11-03 11:39:28 +0300

    feat: create interface for event

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-<<<<<<< HEAD
 import Login from './page/login/Login';
 
 function App() {
@@ -10,17 +9,7 @@ function App() {
         <Route path="/login" element={<Login/>}/>
       </Routes>
   )
-=======
-import './css/EventSelect.css';
-import SelectEvent from './components/EventSelect';
 
-function App() {
-  return (
-    <div className="App">
-     <SelectEvent/>
-    </div>
-  );
->>>>>>> d46f40a (feat: create interface for event)
 }
 
 export default App;
